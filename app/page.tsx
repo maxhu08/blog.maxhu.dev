113983f73560dff64b5497252674db5f78929e5a
maxhu08 <contact@maxhu.dev>
2025-01-05 17:05:30 -0600

    add homepage stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { WarningTriangle } from "iconoir-react";
-
 const Page = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-4 justify-center items-center">
-      <WarningTriangle className="text-yellow-500 text-4xl" />
-      <p className="text-2xl">Under construction!</p>
+    <div className="grid min-h-screen place-items-center">
+      <div className="grid place-items-center grid-flow-row gap-4 max-w-[50ch]">
+        <span className="text-emerald-500 font-semibold text-2xl md:text-4xl">
+          blog.maxhu.dev
+        </span>
+        <p className="text-center">
+          This is my blog website. I'm going to write stuff about coding, and
+          other stuff I'm interested in at the moment. This site is still a work
+          in progress.
+        </p>
+        <div className="w-8 h-8">
+          <img src="/assets/mh.png" alt="icon" />
+        </div>
+      </div>
     </div>
   );
 };
